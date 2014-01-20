3eb6ded609a7c14ce07a77f4cb1774d2d1f5db25
Alex N Jose <alexnj@gmail.com>
2014-01-20 12:37:29 -0800

    Added a preference intent to filter results.

--- gen/com/alexnj/imagesearch/R.java
+++ gen/com/alexnj/imagesearch/R.java
@@ -8,6 +8,14 @@
 package com.alexnj.imagesearch;
 
 public final class R {
+    public static final class array {
+        public static final int pref_color_titles=0x7f070002;
+        public static final int pref_color_values=0x7f070003;
+        public static final int pref_size_titles=0x7f070000;
+        public static final int pref_size_values=0x7f070001;
+        public static final int pref_type_titles=0x7f070004;
+        public static final int pref_type_values=0x7f070005;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,18 +25,19 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_filter=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int btnSearch=0x7f080001;
-        public static final int etSearchQuery=0x7f080000;
-        public static final int gvResults=0x7f080002;
-        public static final int ivLargeImage=0x7f080003;
+        public static final int action_settings=0x7f0a0004;
+        public static final int btnSearch=0x7f0a0001;
+        public static final int etSearchQuery=0x7f0a0000;
+        public static final int gvResults=0x7f0a0002;
+        public static final int ivLargeImage=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_search=0x7f030000;
@@ -36,16 +45,22 @@ public final class R {
         public static final int item_image_result=0x7f030002;
     }
     public static final class menu {
-        public static final int search=0x7f070000;
-        public static final int single_image=0x7f070001;
+        public static final int search=0x7f090000;
+        public static final int single_image=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050005;
-        public static final int image_search_label=0x7f050002;
-        public static final int search=0x7f050003;
-        public static final int title_activity_single_image=0x7f050004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060005;
+        public static final int image_search_label=0x7f060002;
+        public static final int pref_color_display_name=0x7f06000a;
+        public static final int pref_header_general=0x7f060007;
+        public static final int pref_site_display_name=0x7f060009;
+        public static final int pref_size_display_name=0x7f060008;
+        public static final int pref_type_display_name=0x7f06000b;
+        public static final int search=0x7f060003;
+        public static final int title_activity_settings=0x7f060006;
+        public static final int title_activity_single_image=0x7f060004;
     }
     public static final class style {
         /** 
@@ -69,10 +84,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int pref_general=0x7f040000;
     }
 }
--- src/com/alexnj/imagesearch/SearchActivity.java
+++ src/com/alexnj/imagesearch/SearchActivity.java
@@ -10,14 +10,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -33,7 +36,7 @@ public class SearchActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
-		
+						
 		setupViews();
 		imageAdapter = new ImageResultArrayAdapter( this, irResults );
 		gvResults.setAdapter( imageAdapter );
@@ -52,6 +55,11 @@ public class SearchActivity extends Activity {
 		});
 	}
 
+	public void onFilterAction(MenuItem mi) {
+		Intent i = new Intent( getApplicationContext(), SettingsActivity.class );
+		startActivity(i);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -67,11 +75,31 @@ public class SearchActivity extends Activity {
 	
 	public void onImageSearch(View v) {
 		String query = etSearchQuery.getText().toString();
+		String prefSize = PreferenceManager.getDefaultSharedPreferences( this ).getString("size","");
+		String prefColor = PreferenceManager.getDefaultSharedPreferences( this ).getString("color","");
+		String prefSite = PreferenceManager.getDefaultSharedPreferences( this ).getString("site","");
+		String prefType = PreferenceManager.getDefaultSharedPreferences( this ).getString("type","");
+		
+		if( prefSize != "" ) {
+			prefSize = "&imgsz=" + prefSize;
+		}
+		if( prefType != "" ) {
+			prefType = "&imgtype=" + prefType;
+		}
+		if( prefColor != "" ) {
+			prefColor = "&imgcolor=" + prefColor;
+		}
+		if( prefSite != "" ) {
+			prefSite = "&as_sitesearch=" + Uri.encode(prefSite);
+		}
 		
 		AsyncHttpClient client = new AsyncHttpClient();
+
+		Log.d( "DEBUG", "http://ajax.googleapis.com/ajax/services/search/images?rsz=8&start=" + 0 
+				+ "&v=1.0&q=" + Uri.encode( query ) + prefSize + prefType + prefColor + prefSite );
 		
 		client.get("http://ajax.googleapis.com/ajax/services/search/images?rsz=8&start=" + 0 
-				+ "&v=1.0&q==" + Uri.encode( query ), 
+				+ "&v=1.0&q=" + Uri.encode( query ) + prefSize + prefType + prefColor + prefSite, 
 			new JsonHttpResponseHandler() {
 				@Override
 				public void onSuccess( JSONObject response ) {
